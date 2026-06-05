31d98939bdc65a2760522fb5f254e55b48614ac7
rupeshstha <46959961+rupeshstha@users.noreply.github.com>
2026-06-05 09:36:25 +0000

    Fix code styling

--- src/Server/Attributes/TaskSupport.php
+++ src/Server/Attributes/TaskSupport.php
@@ -8,11 +8,11 @@ use Attribute;
 use Laravel\Mcp\Enums\TaskSupport as TaskSupportEnum;
 
 #[Attribute(Attribute::TARGET_CLASS)]
-  class TaskSupport
-  {
-        public function __construct(
-                  public TaskSupportEnum $value = TaskSupportEnum::Forbidden,
-              ) {
-                  //
-        }
-  }
+class TaskSupport
+{
+    public function __construct(
+        public TaskSupportEnum $value = TaskSupportEnum::Forbidden,
+    ) {
+        //
+    }
+}
